ee45920699b731717bf3444b56ff29a6a2381d04
PressureDraper <warmarines00@hotmail.com>
2026-06-10 16:57:32 -0600

    feat(frontend): progress to login page and R2 config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+    /* config options here */
+    reactCompiler: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'pub-2340d562e49941639edaa262ef4996bc.r2.dev'
+            }
+        ]
+    }
 };
 
 export default nextConfig;
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,8 +1,19 @@
+import Image from 'next/image'
 
 export default function LoginPage() {
     return (
-        <main className="flex-1">
-            
+        <main className="flex-1 bg-neutral-50 flex">
+            <div className="bg-neutral-100 w-4/9 p-5 text-center">
+                <h1 className="font-normal text-7xl font-display">Valtierrè</h1>
+            </div>
+            <div className="w-5/9 relative">
+                <Image
+                    src="https://pub-2340d562e49941639edaa262ef4996bc.r2.dev/login/login.webp"
+                    fill
+                    className='object-cover'
+                    alt="Login Image"
+                />
+            </div>
         </main>
     )
 }
