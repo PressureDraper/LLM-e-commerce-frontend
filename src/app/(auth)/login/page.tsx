46533b031612efcca547c2bd39a4233e181ba2e8
PressureDraper <warmarines00@hotmail.com>
2026-06-19 16:43:52 -0600

    feat(frontend): main UI login form complete, pending to add OAuth 2.0 UI and both authentication flows

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,14 +1,31 @@
+'use client';
 import { LoginForm } from '@/modules/auth/components/LoginForm'
+import { RegistForm } from '@/modules/auth/components/RegistForm';
 import Image from 'next/image'
+import { useState } from 'react';
 
 export default function LoginPage() {
+    const [activeForm, setActiveForm] = useState<string>("login");
+
     return (
         <main className="flex-1 bg-neutral-50 flex">
-            <div className="w-full lg:w-4/9 p-5 flex flex-col items-center justify-center">
-                <h1 className="font-normal text-7xl font-display">Valtierrè</h1>
-                <LoginForm />
+            <div className="w-full lg:w-7xl p-5 flex flex-col items-center justify-center">
+                <h1 className="font-normal text-7xl md:text-8xl font-display">Valtierrè</h1>
+                <div className="max-w-130 mx-auto w-full h-full mt-20 md:mt-40 flex flex-col gap-10">
+                    <div className="flex justify-between">
+                        <button onClick={() => setActiveForm("login")}>
+                            <span className={`text-md font-body uppercase font-extralight ${activeForm === 'signin' && 'cursor-pointer text-gray-400'} transition-colors duration-400`}>Iniciar Sesión</span>
+                        </button>
+                        <button onClick={() => setActiveForm("signin")}>
+                            <span className={`text-md font-body uppercase font-extralight ${activeForm === 'login' && 'cursor-pointer text-gray-400'} transition-colors duration-400`}>Registrarse</span>
+                        </button>
+                    </div>
+                    {
+                        activeForm === 'login' ? <LoginForm /> : <RegistForm />
+                    }
+                </div>
             </div>
-            <div className="w-5/9 relative hidden lg:block">
+            <div className="w-full relative hidden lg:block">
                 <Image
                     src="https://pub-2340d562e49941639edaa262ef4996bc.r2.dev/login/login.webp"
                     loading='eager'
--- src/modules/auth/components/LoginForm.tsx
+++ src/modules/auth/components/LoginForm.tsx
@@ -1,20 +1,7 @@
-'use client';
-
-import { useState } from "react"
-
 export const LoginForm = () => {
-    const [activeForm, setActiveForm] = useState<string>("login");
 
     return (
-        <div className="max-w-130 m-auto w-full flex flex-col gap-10">
-            <div className="flex justify-between">
-                <button onClick={() => setActiveForm("login")}>
-                    <span className={`text-md font-body uppercase font-extralight ${activeForm === 'signin' && 'cursor-pointer text-gray-400'} transition-colors duration-400`}>Iniciar Sesión</span>
-                </button>
-                <button onClick={() => setActiveForm("signin")}>
-                    <span className={`text-md font-body uppercase font-extralight ${activeForm === 'login' && 'cursor-pointer text-gray-400'} transition-colors duration-400`}>Registrarse</span>
-                </button>
-            </div>
+        <>
             <div className="relative">
                 <input
                     id="email"
@@ -30,6 +17,33 @@ export const LoginForm = () => {
                     EMAIL
                 </label>
             </div>
-        </div>
+            <div className="relative">
+                <input
+                    id="password"
+                    type="password"
+                    placeholder=" "
+                    className="peer w-full border-0 border-b border-neutral-400 bg-transparent py-1 font-light focus:outline-none focus:border-neutral-600 transition-colors duration-300"
+                />
+
+                <label
+                    htmlFor="password"
+                    className="absolute left-0 -top-3 text-xs text-gray-400 font-body font-light transition-all duration-300 peer-placeholder-shown:top-3 peer-placeholder-shown:text-xs peer-focus:-top-3 peer-focus:text-xs"
+                >
+                    CONTRASEÑA
+                </label>
+
+                <div className="w-full flex justify-end mt-2">
+                    <span className="font-light text-xs underline underline-offset-2 cursor-pointer">¿Olvidaste tu contraseña?</span>
+                </div>
+            </div>
+            <div className="w-full text-center border rounded p-1 cursor-pointer hover:bg-neutral-200 transition-colors duration-200">
+                <button className="font-body font-extralight cursor-pointer">CONTINUAR</button>
+            </div>
+            <div className="w-full flex items-center -mt-2">
+                <div className="flex-1 border-t border-gray-300"></div>
+                <span className="px-4 text-gray-500">Ó</span>
+                <div className="flex-1 border-t border-gray-300"></div>
+            </div>
+        </>
     )
 }
